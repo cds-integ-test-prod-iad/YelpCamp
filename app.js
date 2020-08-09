24131417f7292edf26cf6d75fb263c09cc614151
NoNickle <zzhao1573@gmail.com>
2020-08-09 00:13:15 +0000

    Add DB

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ const 	commentRoutes    = require("./routes/comments"),
 		indexRoutes      = require("./routes/index")
 
 // seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://zzted:45687asd@cluster0.p6lhs.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
